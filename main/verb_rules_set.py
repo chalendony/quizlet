a5e5e04ec5dd4e4b220e70b257b19a938168c944
Avaré Stewart <avare.stewart@gmail.com>
2020-05-29 00:14:30 +0200

    formating content for verb set...needs refactoring

--- main/verb_rules_set.py
+++ main/verb_rules_set.py
@@ -23,15 +23,82 @@ class Verb_Cards:
             select  * from german 
             ;
             """
-        # ====== Reading table ======
-        # Reading PostgreSQL table into a pandas DataFrame
         df = pd.read_sql(query, self.engine)
-        print(df.head)
+        pd.set_option('display.max_rows', 20)
+        pd.set_option('display.max_columns', 20)
+        pd.set_option('display.width', 1000)
+        # pivot
+        group = df.groupby('term')
+        terms = [key for key, _ in group]
+        print(terms)
 
+        g = group.get_group('abarbeiten')
+
+
+        blas = group.get_group('abarbeiten').ktype
+        blas.values
+        print (type(blas))
+        if 'verb' in set(blas):
+            print(f"Hallo world :" )
+
+            d = group.get_group('abarbeiten')
+
+            #print(d.loc[d['ktype'] == 'verb'])
+
+            # check verb
+            if ('|' in str(d.loc[d['ktype'] == 'verb'].sense)):
+                print("found it")
+                # order: sense, translation, prep, reverso, examples, phrase
+                sen = clean_unicode(d.loc[d['ktype'] == 'verb'].sense.to_string(index=False))
+                val = d.loc[d['ktype'] == 'verb'].value
+
+                #### TODO: Pack this code into a function
+                bval = ""
+                for i in val: # hack:  gets the first and only  element of the series????
+                    s = json.loads(i)
+                    for j in s:
+                        en = clean_unicode(j['en'])
+                        de = clean_unicode(j['de'])
+                        bval = bval + de + "  "
+
+                rev = d.loc[d['ktype'] == 'reverso'].value
+                brev = ""
+                for i in rev:
+                    brev = brev + i + "  "
+
+                exa = d.loc[d['ktype'] == 'example'].value.values
+                bexa = ""
+                for i in exa:
+                    bexa = bexa + + i + "  "
+
+                phr = d.loc[d['ktype'] == 'phrase'].value.values
+                bphr = ""
+                for i in phr:
+                    bphr = bphr + + i + "  "
+                phr = d.loc[d['ktype'] == 'phrase'].value.values
+                print(f"sense***** {sen}")
+                print(f"value***** {bval}")
+                print(f"reverso***** {brev}")
+                print(f"example***** {bexa}")
+                print(f"phrase***** {bphr}")
+
+            # concate
+
+
+
+        # print
+
+    def format_sense(self, lst):
+        tmp = []
+        for i in lst:
+            clean = clean_unicode(i['en'])
+            tmp.append(clean.strip())
+        str = '['+ '; '.join(tmp) + ']'
+        return str
 
 
     def shutdown(self):
-        #pg.cur.close()
+        #self.engine.
         #pg.conn.close()
         pass
 
@@ -39,4 +106,4 @@ if __name__ == "__main__":
 
     pg = Verb_Cards()
     pg.create()
-    pg.shutdown()
+    #pg.shutdown()
